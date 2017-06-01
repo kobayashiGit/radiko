e1e84cfa77810ca66e90e95b7072117b95eae26e
小林 洋生 <hipuohipuo@gmail.com>
2017-06-01 15:03:43 +0900

    no message

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //var_dump( exec('ls', $out, $ret) );
 //print_r( $out );
 //print posix_getpwuid(posix_geteuid())['name'];
-exec("nohup ./radiko.sh FMT > /dev/null &",$out);
+exec("./radiko.sh FMT",$out);
 print_r($out);
 exit;
 session_start();
