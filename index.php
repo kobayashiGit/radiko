6b9fcea6d7d0d4a502fa037fbeb34641f5a07f67
小林 洋生 <hipuohipuo@gmail.com>
2017-06-01 11:18:49 +0900

    no message

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 //var_dump( exec('ls', $out, $ret) );
 //print_r( $out );
-print posix_getpwuid(posix_geteuid())['name'];
-exec("sudo radiko.sh FMT",$out);
-print_r($out);
-exit;
+//print posix_getpwuid(posix_geteuid())['name'];
+//exec("sudo radiko.sh FMT",$out);
+//print_r($out);
+//exit;
 session_start();
 
 if(isset($_GET["id"])){
